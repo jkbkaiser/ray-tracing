f585ff17c35b078fce740b8cf8535dc820ed42ab
Jakob Kaiser <jakob.j.jeep@gmail.com>
2024-08-01 12:57:58 +0200

    Completed Chapter 5

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,28 @@
 #include "color.h"
 #include "ray.h"
+#include "vec3.h"
 
 #include <iostream>
 
-color ray_color(const ray& r) { return color(0, 0, 0); }
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    auto cq = center - r.origin();
+
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), cq);
+    auto c = dot(cq, cq) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    return (discriminant >= 0);
+}
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, 1), 0.5, r))
+        return color(1, 0, 0);
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
     auto aspect_ratio = 16.0 / 9.0;
